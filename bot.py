e97e083f5fae90ceeffdbfdd1eff5e7d26b05e48
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-03-14 18:39:11 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ Welcome To {Config.ACCOUNT_GEN_NAME} Account Generator Bot
 To Know About commands type:
 /cmds
 
-Bot Made With ❤️ By @MadPremiumo**
+Bot Made With ❤️ By @MadPremium**
 """ 
     await event.reply(user_text) 
     
